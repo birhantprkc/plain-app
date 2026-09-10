df67eeafa88d45d63cdbbf8771f1271ade9f4ad5
iSmartCoding <ismartcoding@gmail.com>
2026-09-11 04:45:18 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,9 @@ android {
                 else -> 0
             }
 
-        val vCode = 694
+        val vCode = 697
         versionCode = vCode - singleAbiNum
-        versionName = "3.3.23"
+        versionName = "3.3.24"
 
         ndk {
             //noinspection ChromeOsAbiSupport
