658288c90c800b31004ce320ef348d515c27deba
iSmartCoding <ismartcoding@gmail.com>
2026-09-18 21:34:39 +0800

    build web code

---
+++ app/src/main/resources/web/assets/tapphone-BM68UiZ3.js
@@ -0,0 +1,1 @@
+import{s as e}from"./temp-D1Kgo3Le.js";var t=t=>{e.emit(`tap_phone`,t)};export{t};
--- app/src/main/resources/web/assets/tapphone-DAOiG3QR.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./temp-BxyXOeiJ.js";var t=t=>{e.emit(`tap_phone`,t)};export{t};
